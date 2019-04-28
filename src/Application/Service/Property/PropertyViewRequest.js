395dee1a86ea4ec6460ce84b2c38a6dde5252529
mrcarlosdev <mrcarlosdev@gmail.com>
2019-04-28 08:48:33 +0200

    Make PropertyViewRequest inmutable

--- src/Application/Service/Property/PropertyViewRequest.js
+++ src/Application/Service/Property/PropertyViewRequest.js
@@ -1,17 +1,21 @@
-module.exports = class PropertyViewRequest {
-  constructor(
-    propertyId,
-    ownerId
-  ) {
+function PropertyViewRequest(propertyId,
+  ownerId) {
     this.propertyId = propertyId;
     this.ownerId = ownerId;
+
+    if (this.constructor === PropertyViewRequest) {
+      Object.freeze(this);
+    }
+    console.log(this.propertyId)
   }
 
-  propertyId() {
-    return this.propertyId;
-  }
+PropertyViewRequest.propertyId = function propertyId() {
+  return this.propertyId;
+};
 
-  ownerId() {
-    return this.ownerId;
-  }
-}
+PropertyViewRequest.ownerId = function ownerId() {
+  return this.ownerId;
+};
+
+module.exports = PropertyViewRequest
+
